20825c1fab5207175e3c12903d07f1871a14c65d
David Toledo <dtoledoz0207@gmail.com>
2020-09-14 12:10:25 -0500

    create-maps-and-directions-pages-and-add-BottomNavigationBar-at-homePage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:qrreader/src/pages/directions_page.dart';
+import 'package:qrreader/src/pages/maps_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndexPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home page'),
-      ),
+      body: _callPage(currentIndexPage),
+      bottomNavigationBar: _createBottomNavigationBar(),
+    );
+  }
+
+
+  Widget _callPage(int currentPage) {
+    switch (currentPage) {
+      case 0: return MapsPage();
+      case 1: return DirectionsPage();
+      default:
+        return MapsPage();
+    }
+  }
+
+  Widget _createBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndexPage,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Maps')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.web),
+          title: Text('urls')
+        )
+      ],
+      onTap: (index) {
+        setState(() {
+          currentIndexPage = index;
+        });
+      },
     );
   }
 }

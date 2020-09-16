2b50da150dba7baecfb7f8c2b5c21906321070e0
David Toledo <dtoledoz0207@gmail.com>
2020-09-16 12:03:46 -0500

    create-database-and-table-Scans

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 class DBProvider {
 
@@ -15,7 +19,24 @@ class DBProvider {
   }
 
   initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
 
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database database, int version) async {
+        await database.execute(
+          'CREATE TABLE Scans ('
+          'id INTEGER PRIMARY KEY,'
+          'type TEXT,'
+          'value TEXT'
+          ')'
+        );
+      }
+    );
   }
 
 }

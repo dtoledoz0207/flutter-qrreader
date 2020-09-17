5b5f80a90c8daf3c48d56187d8c0af262d9fefc6
David Toledo <dtoledoz0207@gmail.com>
2020-09-17 11:59:20 -0500

    create-methods-to-getScans-by-id-type-and-all

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:qrreader/src/models/scan_model.dart';
 
 
 class DBProvider {
@@ -39,4 +40,53 @@ class DBProvider {
     );
   }
 
+  
+  // ***+***** CREATE RECORDS ****************
+
+  /*
+  newScanRaw(ScanModel newScan) async {
+    final db = await database;
+    final result = await db.rawInsert(
+      "INSERT INTO Scans (id, type, value) "
+      "VALUES (${newScan.id}, '${newScan.type}', '${newScan.value}')"
+    );
+    return result;
+  }
+  */
+
+  newScan(ScanModel newScan) async {
+    final db = await database;
+
+    final result  = await db.insert('Scans', newScan.toJson());
+
+    return result;
+  }
+
+  Future<ScanModel> getScanById(int id) async {
+    final db = await database;
+
+    final result = await db.query('Scans', where: 'id = ?', whereArgs: [id]);
+
+    return result.isNotEmpty ? ScanModel.fromJson(result.first) : null; 
+  }
+
+  Future<List<ScanModel>> getAllScans() async {
+    final db = await database;
+    final result = await db.query('Scans');
+
+    List<ScanModel> list = result.isNotEmpty ? result.map((scan) => ScanModel.fromJson(scan)).toList() : [];
+
+    return list;
+  }
+
+  Future<List<ScanModel>> getScansByType(String type) async {
+    final db = await database;
+
+    //final result = await db.query('Scans', where: 'type = ?', whereArgs: [type]);
+    final result = await db.rawQuery("SELECT * FROM Scans WHERE type = '$type'");
+
+    List<ScanModel> list = result.isNotEmpty ? result.map((scan) => ScanModel.fromJson(scan)).toList() : [];
+
+    return list;
+  }
 }

4fcfc45d19745c47dfbdfa7706644302d9aee0b3
David Toledo <dtoledoz0207@gmail.com>
2020-09-21 12:02:14 -0500

    create-scansBloc-file

---
+++ lib/src/bloc/scans_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:qrreader/src/models/scan_model.dart';
+
+class ScansBloc {
+
+  static final ScansBloc _singleton = new ScansBloc._internal();
+
+  factory ScansBloc() {
+    return _singleton;
+  }
+
+  ScansBloc._internal() {
+    // Get scans from DB
+
+  }
+
+  final _scansController = StreamController<List<ScanModel>>.broadcast();
+
+  Stream<List<ScanModel>> get scansStream => _scansController.stream;
+
+  dispose() {
+    _scansController?.close();
+  }
+
+}

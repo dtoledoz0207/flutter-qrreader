48361c2b5e91c7ee1aa57dc955891b22f4b7d2a4
David Toledo <dtoledoz0207@gmail.com>
2020-09-22 10:53:37 -0500

    create-methods-to-management-streamController-scansBloc

--- lib/src/bloc/scans_bloc.dart
+++ lib/src/bloc/scans_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:qrreader/src/models/scan_model.dart';
+import 'package:qrreader/src/providers/db_provider.dart';
 
 class ScansBloc {
 
@@ -11,7 +12,7 @@ class ScansBloc {
 
   ScansBloc._internal() {
     // Get scans from DB
-
+    getScans();
   }
 
   final _scansController = StreamController<List<ScanModel>>.broadcast();
@@ -20,6 +21,25 @@ class ScansBloc {
 
   dispose() {
     _scansController?.close();
+  }
+
+  getScans() async {
+    _scansController.sink.add(await DBProvider.db.getAllScans());
+  }
+
+  addNewScan(ScanModel scan) async {
+    await DBProvider.db.newScan(scan);
+    getScans();
+  }
+
+  deleteScan(int id) async {
+    await DBProvider.db.deleteScan(id);
+    getScans();
+  }
+
+  deleteAllScans() async {
+    await DBProvider.db.deleteAllScans();
+    getScans();
   }
 
 }

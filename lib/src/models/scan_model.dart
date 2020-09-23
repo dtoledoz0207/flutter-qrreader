c64736102530ebd0f8385c70c29f88acc340724e
David Toledo <dtoledoz0207@gmail.com>
2020-09-23 11:58:37 -0500

    Add flutter_map package
    This adds the flutter_map package to pubspec.yaml file and
    creates a new method in scan_model file to return LatLng and
    creates the functionality to show the mapbox map in mapPage.

--- lib/src/models/scan_model.dart
+++ lib/src/models/scan_model.dart
@@ -1,3 +1,4 @@
+import 'package:latlong/latlong.dart';
 
 class ScanModel {
     
@@ -24,4 +25,13 @@ class ScanModel {
         "type": type,
         "value": value,
     };
+
+    LatLng getLatLng() {
+      final latlng = value.substring(4).split(',');
+
+      final lat = double.parse(latlng[0]);
+      final lng = double.parse(latlng[1]);
+
+      return LatLng(lat, lng);
+    }
 }
--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+
 import 'package:qrreader/src/models/scan_model.dart';
 
 
@@ -19,8 +21,30 @@ class MapPage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text(scan.value),
+        child: _createFlutterMap(scan)
       ),
+    );
+  }
+
+  Widget _createFlutterMap(ScanModel scan) {
+    return FlutterMap(
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 10.0
+      ),
+      layers: [
+        _createMap()
+      ],
+    );
+  }
+
+  _createMap() {
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken':'pk.eyJ1IjoiZHRvbGVkb3oiLCJhIjoiY2tmZmwwbmFxMDB1ZjJzb3ZhdmtvandtbCJ9.CjwlVoy9uQIx9hQY9DL9FA',
+        'id': 'mapbox/streets-v11'
+      }
     );
   }
 }

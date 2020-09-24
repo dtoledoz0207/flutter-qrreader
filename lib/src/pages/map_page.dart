4f8702093fde89bb7d90c534c75707bd243d3d0d
David Toledo <dtoledoz0207@gmail.com>
2020-09-24 11:47:56 -0500

    Change to StatfulWidget at mapPage
    This change the mapPage from StatelessWidget to StatefulWidget to
    can change the mapType and show the new mapType in the screen.

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -4,9 +4,16 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:qrreader/src/models/scan_model.dart';
 
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+
+class _MapPageState extends State<MapPage> {
 
   final mapCtrl = MapController();
+  String mapType = 'streets-v11';
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +35,7 @@ class MapPage extends StatelessWidget {
       body: Center(
         child: _createFlutterMap(scan)
       ),
-      floatingActionButton: _createFloatingActionButton(context),
+      floatingActionButton: _createFloatingActionButton(context, scan),
     );
   }
 
@@ -47,12 +54,11 @@ class MapPage extends StatelessWidget {
   }
 
   _createMap() {
-    // streets-v11, dark-v10, light-v10, outdoors-v11, satellite-v9, satellite-streets-v11
     return TileLayerOptions(
       urlTemplate: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
       additionalOptions: {
         'accessToken':'pk.eyJ1IjoiZHRvbGVkb3oiLCJhIjoiY2tmZmwwbmFxMDB1ZjJzb3ZhdmtvandtbCJ9.CjwlVoy9uQIx9hQY9DL9FA',
-        'id': 'mapbox/streets-v11'
+        'id': 'mapbox/$mapType'
       }
     );
   }
@@ -72,11 +78,33 @@ class MapPage extends StatelessWidget {
     );
   }
 
-  Widget _createFloatingActionButton(BuildContext context) {
+  Widget _createFloatingActionButton(BuildContext context, ScanModel scan) {
     return FloatingActionButton(
       child: Icon(Icons.repeat),
       backgroundColor: Theme.of(context).primaryColor,
-      onPressed: () {}
+      onPressed: () {
+        // streets-v11, dark-v10, light-v10, outdoors-v11, satellite-v9, satellite-streets-v11
+
+        setState(() {
+          if (mapType == 'streets-v11') {
+            mapType = 'dark-v10';
+          } else if (mapType == 'dark-v10') {
+            mapType = 'light-v10';
+          } else if (mapType == 'light-v10') {
+            mapType = 'outdoors-v11';
+          } else if (mapType == 'outdoors-v11') {
+            mapType = 'satellite-streets-v11';
+          } else {
+            mapType = 'streets-v11';
+          }
+        });
+
+        mapCtrl.move(scan.getLatLng(), 20);
+
+        Future.delayed(Duration(milliseconds: 50), () {
+          mapCtrl.move(scan.getLatLng(), 15.0);
+        });
+      }
     );
   }
 }
